004a6689f4e346376dc9c6b8b69b2f623184e22c
brunobus <75343075+brunobus@users.noreply.github.com>
2020-12-03 01:55:26 +0100

    dodan meni na MainActivity

---
+++ app/src/main/java/com/example/notes_app/LoginRegisterActivity.java
@@ -0,0 +1,18 @@
+package com.example.notes_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_register);
+    }
+
+    public void handleLoginRegister(View view) {
+    }
+}
